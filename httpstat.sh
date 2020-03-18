3036cac55ff834d98ab8fd81690abe47998680ba
GovindPalakkal <62349500+GovindPalakkal@users.noreply.github.com>
2020-03-18 22:26:04 +0530

    Create httpstat.sh

---
+++ httpstat.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#color_codes
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+
+echo "${red}
+|------------------------------xxxxxxxx-------------------------------------------|
+|           _   _ _____ _____ ____    ____ _____  _  _____                        |
+|          | | | |_   _|_   _|  _ \  / ___|_   _|/ \|_   _|                       |
+|          | |_| | | |   | | | |_) | \___ \ | | / _ \ | |                         |
+|          |  _  | | |   | | |  __/   ___) || |/ ___ \| |                         |
+|          |_| |_| |_|   |_| |_|     |____/ |_/_/   \_\_|                         |
+|                                     - T00l_by_HBM_D3LT4                         |
+|------------------------------xxxxxxxx ------------------------------------------|
+
+"
+
+echo "${yellow}
+----------------------------------------------"
+echo "|  [+]Scanning starting ..........            |"
+echo  "---------------------------------------------"
+
+for site in $(cat list.txt);
+do #starts here....
+   curl -s -o /dev/null -I -w  "%{http_code}" https://$site --max-time 5;  printf " - $site \n" & done > alive.txt
+   #using curl to get the request and fetch out the status code
+echo "${green}
+----------------------------------------------"
+echo "  [+]showing.. result..... please wait....... "
+echo "----------------------------------------------"
+ cat alive.txt
+
